19924b59ee9de5406e416e108b109fe06cf3e5e7
Nicolas Dittlo <newlab.plugins@gmail.com>
2024-12-31 15:23:53 +0100

    added pack script

---
+++ src/plugins/NL-Denoiser/pack-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ./Builds/LinuxMakefile/build/NL-Denoiser.so ../../../pack/Linux/NL-Denoiser-linux-amd64/vst2
+cp -R ./Builds/LinuxMakefile/build/NL-Denoiser.vst3 ../../../pack/Linux/NL-Denoiser-linux-amd64/vst3
