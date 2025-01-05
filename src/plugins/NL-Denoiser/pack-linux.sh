78d812937b29b5af4322595d1af03cce6775dca9
Nicolas Dittlo <newlab.plugins@gmail.com>
2025-01-05 16:41:11 +0100

    added LV2 plugin format

--- src/plugins/NL-Denoiser/pack-linux.sh
+++ src/plugins/NL-Denoiser/pack-linux.sh
@@ -2,4 +2,5 @@
 
 cp ./Builds/LinuxMakefile/build/NL-Denoiser.so ../../../pack/Linux/NL-Denoiser-linux-amd64/vst2
 cp -R ./Builds/LinuxMakefile/build/NL-Denoiser.vst3 ../../../pack/Linux/NL-Denoiser-linux-amd64/vst3
+cp -R ./Builds/LinuxMakefile/build/NL-Denoiser.lv2 ../../../pack/Linux/NL-Denoiser-linux-amd64/lv2
 cp ../../../doc/denoiser/NL-Denoiser_manual.pdf ../../../pack/Linux/NL-Denoiser-linux-amd64
