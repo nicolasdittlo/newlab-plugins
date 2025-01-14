1671117b623b20c55d993922808def34ccaf0153
Nicolas Dittlo <newlab.plugins@gmail.com>
2025-01-14 15:18:31 +0100

    updated pack script for air

--- src/plugins/NL-Air/pack-linux.sh
+++ src/plugins/NL-Air/pack-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./Builds/LinuxMakefile/build/NL-Denoiser.so ../../../pack/Linux/NL-Denoiser-linux-amd64/vst2
-cp -R ./Builds/LinuxMakefile/build/NL-Denoiser.vst3 ../../../pack/Linux/NL-Denoiser-linux-amd64/vst3
-cp -R ./Builds/LinuxMakefile/build/NL-Denoiser.lv2 ../../../pack/Linux/NL-Denoiser-linux-amd64/lv2
-cp ../../../doc/denoiser/NL-Denoiser_manual.pdf ../../../pack/Linux/NL-Denoiser-linux-amd64
+cp ./Builds/LinuxMakefile/build/NL-Air.so ../../../pack/Linux/NL-Air-linux-amd64/vst2
+cp -R ./Builds/LinuxMakefile/build/NL-Air.vst3 ../../../pack/Linux/NL-Air-linux-amd64/vst3
+cp -R ./Builds/LinuxMakefile/build/NL-Air.lv2 ../../../pack/Linux/NL-Air-linux-amd64/lv2
+cp ../../../doc/denoiser/NL-Air_manual.pdf ../../../pack/Linux/NL-Air-linux-amd64
