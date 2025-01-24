d1a5b500a12d206a65942a5e03f0ddacf20e62d5
Nicolas Dittlo <nicolas.dittlo@akkodis.com>
2025-01-24 15:39:17 +0100

    fixed pack scripts windows + added plugin VST ID

--- src/plugins/BL_Air/pack-win.bat
+++ src/plugins/BL_Air/pack-win.bat
@@ -1,5 +1,5 @@
-copy .\Installer\BL_Denoiser-*-Installer.exe ..\..\..\pack\Windows\BL_Denoiser
-copy ..\..\..\doc\denoiser\BL_Denoiser_manual.pdf ..\..\..\pack\Windows\BL_Denoiser
-copy .\Builds\VisualStudio2019\x64\Release\VST\BL_Denoiser.dll ..\..\..\pack\Windows\BL_Denoiser\vst2
-xcopy .\Builds\VisualStudio2019\x64\Release\VST3\BL_Denoiser.vst3 ..\..\..\pack\Windows\BL_Denoiser\vst3\BL_Denoiser.vst3\ /E /Y
-xcopy .\Builds\VisualStudio2019\x64\Release\AAX\BL_Denoiser.aaxplugin ..\..\..\pack\Windows\BL_Denoiser\aax\BL_Denoiser.aaxplugin\ /E /Y
+copy .\Installer\BL_Air-*-Installer.exe ..\..\..\pack\Windows\BL_Air
+copy ..\..\..\doc\air\BL_Air_manual.pdf ..\..\..\pack\Windows\BL_Air
+copy .\Builds\VisualStudio2019\x64\Release\VST\BL_Air.dll ..\..\..\pack\Windows\BL_Air\vst2
+xcopy .\Builds\VisualStudio2019\x64\Release\VST3\BL_Air.vst3 ..\..\..\pack\Windows\BL_Air\vst3\BL_Air.vst3\ /E /Y
+xcopy .\Builds\VisualStudio2019\x64\Release\AAX\BL_Air.aaxplugin ..\..\..\pack\Windows\BL_Air\aax\BL_Air.aaxplugin\ /E /Y
