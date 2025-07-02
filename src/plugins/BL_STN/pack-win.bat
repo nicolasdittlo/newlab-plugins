7281b16093ebb353d409ff12ea967ea79f117ca1
Nicolas Dittlo <nicolas.dittlo@akkodis.com>
2025-07-02 16:49:27 +0200

    STN: fixed pack script on Windows

--- src/plugins/BL_STN/pack-win.bat
+++ src/plugins/BL_STN/pack-win.bat
@@ -1,5 +1,5 @@
 copy .\Installer\BL_STN-*-Installer.exe ..\..\..\pack\Windows\BL_STN
-copy ..\..\..\doc\air\BL_STN_manual.pdf ..\..\..\pack\Windows\BL_STN
+copy ..\..\..\doc\STN\BL_STN_manual.pdf ..\..\..\pack\Windows\BL_STN
 copy .\Builds\VisualStudio2019\x64\Release\VST\BL_STN.dll ..\..\..\pack\Windows\BL_STN\vst2
 xcopy .\Builds\VisualStudio2019\x64\Release\VST3\BL_STN.vst3 ..\..\..\pack\Windows\BL_STN\vst3\BL_STN.vst3\ /E /Y
 xcopy .\Builds\VisualStudio2019\x64\Release\AAX\BL_STN.aaxplugin ..\..\..\pack\Windows\BL_STN\aax\BL_STN.aaxplugin\ /E /Y
