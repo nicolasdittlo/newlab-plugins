6e82297632c5a3eaacddec231c619afdba1fe7e9
Nicolas Dittlo <zebrafish@Nicolass-MacBook-Air.local>
2025-07-03 19:18:44 +0200

    STN: fixed pack mac script

---
+++ pack/Mac/BL_STN/README.txt
@@ -0,0 +1,3 @@
+- launch the .pkg installer
+or
+- copy the files manually from aax, au, vst2, vst3 to your plugins directories
--- src/plugins/BL_STN/pack-mac.sh
+++ src/plugins/BL_STN/pack-mac.sh
@@ -4,5 +4,5 @@ cp -R ./Builds/MacOSX/build/Release/BL_STN.vst ../../../pack/Mac/BL_STN/vst2
 cp -R ./Builds/MacOSX/build/Release/BL_STN.vst3 ../../../pack/Mac/BL_STN/vst3
 cp -R ./Builds/MacOSX/build/Release/BL_STN.component ../../../pack/Mac/BL_STN/au
 cp -R ./Builds/MacOSX/build/Release/BL_STN.aaxplugin ../../../pack/Mac/BL_STN/aax
-cp ../../../doc/air/BL_STN_manual.pdf ../../../pack/Mac/BL_STN
+cp ../../../doc/STN/BL_STN_manual.pdf ../../../pack/Mac/BL_STN
 cp ./Installer/BL_STN-v7.0.2-installer.pkg ../../../pack/Mac/BL_STN
