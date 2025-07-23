ccf91c836425277d3026eb4d1f3af3397bdd21d0
Nicolas Dittlo <newlab.plugins@gmail.com>
2025-07-23 12:57:31 +0200

    STN: increassed version number for installers and scripts

--- src/plugins/BL_STN/make-dmg-mac.sh
+++ src/plugins/BL_STN/make-dmg-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hdiutil create ../../../pack/Mac/BL_STN-v7.0.2.dmg -format UDZO -srcfolder ../../../pack/Mac/BL_STN -ov -anyowners -volname BL_STN
+hdiutil create ../../../pack/Mac/BL_STN-v7.0.3.dmg -format UDZO -srcfolder ../../../pack/Mac/BL_STN -ov -anyowners -volname BL_STN
--- src/plugins/BL_STN/pack-mac.sh
+++ src/plugins/BL_STN/pack-mac.sh
@@ -5,4 +5,4 @@ cp -R ./Builds/MacOSX/build/Release/BL_STN.vst3 ../../../pack/Mac/BL_STN/vst3
 cp -R ./Builds/MacOSX/build/Release/BL_STN.component ../../../pack/Mac/BL_STN/au
 cp -R ./Builds/MacOSX/build/Release/BL_STN.aaxplugin ../../../pack/Mac/BL_STN/aax
 cp ../../../doc/STN/BL_STN_manual.pdf ../../../pack/Mac/BL_STN
-cp ./Installer/BL_STN-v7.0.2-installer.pkg ../../../pack/Mac/BL_STN
+cp ./Installer/BL_STN-v7.0.3-installer.pkg ../../../pack/Mac/BL_STN
